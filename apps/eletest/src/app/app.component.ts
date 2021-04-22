f4b3f5f060f5b75e6f78fc4aac994dc388df1fe1
Jo Hanna Pearce <jo@nrwl.io>
2021-04-22 16:23:02 +0100

    initial commit

---
+++ apps/eletest/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elements-and-cypress-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'eletest';
+}
